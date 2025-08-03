3cb4495d9a3d578226e9cb7d4b75256c8e63c323
purestanley <stanleychidiebere67@gmail.com>
2025-08-03 12:57:06 +0100

    Removed broken submodule reference for python-app

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"  
 }
 
-resource "aws_instance" "web_server" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  associate_public_ip_address = true
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
   }
 }
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
